d789e2886c4b71500a66adbeaf89b240b4419e99
ryan <rquinan12@gmail.com>
2024-05-06 11:11:07 +0800

    seventh commit

--- kaizen_web/settings.py
+++ kaizen_web/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'kaizen_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['kaizen_web/templates', 'users/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
